3096d085393cc7a9897504cf6a31756c1a3ce519
ksimons <kksimons@gmail.com>
2025-01-11 14:15:48 -0700

    added blink

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,5 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = { "saghen/blink.cmp" },
   opts = {
     inlay_hints = { enabled = false },
     servers = { eslint = {} },
@@ -15,4 +16,13 @@ return {
       end,
     },
   },
+  config = function(_, opts)
+    local lspconfig = require("lspconfig")
+    for server, config in pairs(opts.servers) do
+      -- passing config.capabilities to blink.cmp merges with the capabilities in your
+      -- `opts[server].capabilities, if you've defined it
+      config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+      lspconfig[server].setup(config)
+    end
+  end,
 }
