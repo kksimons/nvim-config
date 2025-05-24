8429570aab74621ba5c2604f2ce6410bbc6bbbd2
ksimons <kksimons@gmail.com>
2025-05-23 19:07:40 -0600

    added elixir lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,7 +3,12 @@ return {
   dependencies = { "saghen/blink.cmp" },
   opts = {
     inlay_hints = { enabled = false },
-    servers = { eslint = {} },
+    servers = {
+      eslint = {},
+      elixirls = {
+        cmd = { "elixir-ls" },
+      },
+    },
     setup = {
       eslint = function()
         require("lazyvim.util").lsp.on_attach(function(client)
@@ -19,8 +24,6 @@ return {
   config = function(_, opts)
     local lspconfig = require("lspconfig")
     for server, config in pairs(opts.servers) do
-      -- passing config.capabilities to blink.cmp merges with the capabilities in your
-      -- `opts[server].capabilities, if you've defined it
       config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
       lspconfig[server].setup(config)
     end
